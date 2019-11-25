367d1c67afe26710e865e9bb4b2232a5d65a9106
jancy-joseph <jancyworkmail@gmail.com>
2019-11-25 09:39:06 -0800

    pgm-1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 1, 2, 4, 4, 5, 5, 6, 6 };
+            UniqueElement.search(arr, 0, arr.Length - 1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/UniqueElement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    static class UniqueElement
+    {
+        public static void search(int[] numberArray, int low, int high)
+        {
+            if (low > high)
+                return;
+            if(low == high){
+                Console.WriteLine("Unique Element is :{0}", numberArray[low]);
+                return;
+            }
+
+            int mid = (low + high) / 2;
+            //if mid index is even
+            if(mid % 2 == 0)
+            {
+                if(numberArray[mid]== numberArray[mid + 1])
+                {
+                    search(numberArray, mid + 2, high);
+                }
+                else
+                {
+                    search(numberArray, low, mid);
+                }
+            }
+            else//  odd mid index
+            {
+               if(numberArray[mid]== numberArray[mid - 1])
+                {
+                    search(numberArray, mid + 1, high);
+                }
+                else
+                {
+                    search(numberArray, low, mid);
+                }
+            }
+        }
+    }
+}
